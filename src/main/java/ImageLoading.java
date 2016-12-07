b5c2bd6cd32e9603c1f813c8a655b7b75681467f
Oleksandr <avpavlov96@gmail.com>
2016-12-07 14:36:23 +0300

    Initial commit

---
+++ src/main/java/ImageLoading.java
@@ -0,0 +1,15 @@
+import java.applet.Applet;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+
+/**
+ * Created by Oleksandr on 07.12.2016.
+ */
+public class ImageLoading extends Applet{
+
+    public void paint(Graphics g){
+        Graphics2D g2 = (Graphics2D)g;
+        
+    }
+
+}
