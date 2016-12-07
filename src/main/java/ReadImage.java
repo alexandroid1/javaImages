7b337165aa1b366706dd95bf5c31f3ed35925419
Oleksandr <avpavlov96@gmail.com>
2016-12-07 18:12:31 +0300

    ref

--- src/main/java/ReadImage.java
+++ src/main/java/ReadImage.java
@@ -9,7 +9,6 @@ import javax.swing.*;
 
 public class ReadImage
 {
-
     final static String spec = "http://www.mkyong.com/image/mypic.jpg";
 
     public static void main( String[] args )
@@ -32,33 +31,21 @@ public class ReadImage
     private static void outPut(Image image) {
         JFrame frame = new JFrame();
         frame.setSize(600, 600);
-        JLabel label = new JLabel(new ImageIcon(image));
-
         JPanel contentPane = new JPanel(){
-
             protected void paintComponent(Graphics g){
-
                 super.paintComponent(g);
-
                 AffineTransform at = AffineTransform.getTranslateInstance(100, 100);
                 at.rotate(Math.toRadians(45));
-
-                BufferedImage Apple = (BufferedImage) inPut("http://www.mkyong.com/image/mypic.jpg");
-
+                BufferedImage Apple = (BufferedImage) inPut(spec);
                 Graphics2D g2d = (Graphics2D) g;
-
                 g2d.drawImage(Apple, at, null);
-
                 g2d=(Graphics2D)g;
                 g2d.setColor(Color.BLACK);
                 g2d.drawLine(20, 20, 360, 20);
             }
         };
         frame.setContentPane(contentPane);
-        frame.add(label);
         frame.setVisible(true);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
-
-
 }
